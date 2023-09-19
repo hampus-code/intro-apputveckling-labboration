7ec2a4ef4106233ef10e4e769d929041bd95bd6b
hampus-code <hampusss96@gmail.com>
2023-09-19 12:05:46 +0200

    Added more infotext on each page + added one more html file

---
+++ logicjs.js
@@ -0,0 +1,32 @@
+function hemsidaButton() {
+    window.location.href = "index.html"
+}
+
+function sida1Button() {
+    window.location.href = "sida1.html"
+}
+
+function sida2Button() {
+    window.location.href = "sida2.html"
+}
+
+function sida3Button() {
+    window.location.href = "sida3.html"
+}
+
+function sida4Button() {
+    window.location.href = "sida4.html"
+}
+
+function sida5Button() {
+    window.location.href = "sida5.html"
+}
+
+
+/* Function för användarens egen star rating */
+function userRating() {
+
+    var result = prompt("Vänligen sätt ditt betyg från 1-10")
+
+    alert(result)
+}
